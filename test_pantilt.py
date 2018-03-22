c30d980edaf7861ecd90d48142a212e1d1f5014c
Dennis Sivia <dennis.sivia@gmail.com>
2018-03-22 17:01:51 +0100

    Adds tests for setting neopixel colors

--- test_pantilt.py
+++ test_pantilt.py
@@ -7,7 +7,7 @@ def f(x):
   return x
 
 
-r1 = np.arange(0,90,1)[::-1]
+r1 = np.arange(0,90,5)[::-1]
 r2 = np.arange(0,40,1)
 
 for p in r1:
@@ -18,12 +18,22 @@ for p in r2:
   pantiltlib.do_tilt(None, p)
   time.sleep(0.05)
 
-while True: 
-  scan_results = pantiltlib.pan_scan(f)
-  print(scan_results)
-  time.sleep
-
-  scan_results = pantiltlib.pan_scan(f)
-  print(scan_results)
-
-  time.sleep(180)
+pantiltlib.set_light_color("red",0)
+time.sleep(2)
+pantiltlib.set_light_color("green",1)
+time.sleep(2)
+pantiltlib.set_light_color("blue",2)
+time.sleep(2)
+pantiltlib.set_light_color("white",None)
+time.sleep(2)
+pantiltlib.set_light_color("xxx")
+time.sleep(2)
+#while True:
+#  scan_results = pantiltlib.pan_scan(f)
+#  print(scan_results)
+#  time.sleep
+#
+#  scan_results = pantiltlib.pan_scan(f)
+#  print(scan_results)
+#
+#  time.sleep(180)
